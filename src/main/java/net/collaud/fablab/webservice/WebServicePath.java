18685a37a48684c9467c6c824457cbe421712cb1
Gaétan Collaud <gaetan.collaud@gmail.com>
2014-05-24 23:38:53 +0200

    constants

---
+++ src/main/java/net/collaud/fablab/webservice/WebServicePath.java
@@ -0,0 +1,14 @@
+package net.collaud.fablab.webservice;
+
+/**
+ *
+ * @author gaetan
+ */
+final public class WebServicePath {
+	public static final String BASE_URL = "api";
+	public static final String DOOR_URL  = "door";
+	public static final String DOOR_REQUEST_OPEN = "requestopen";
+	
+	public static final String PARAM_TOKEN = "token";
+	public static final String PARAM_RFID = "rfid";
+}
