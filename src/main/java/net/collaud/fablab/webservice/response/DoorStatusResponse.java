f4b78b103a8fdc3c071340260eb8428e25bc9e56
Gaétan Collaud <gaetan.collaud@gmail.com>
2014-06-01 18:46:31 +0200

    Add door status Rest response

---
+++ src/main/java/net/collaud/fablab/webservice/response/DoorStatusResponse.java
@@ -0,0 +1,36 @@
+package net.collaud.fablab.webservice.response;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author gaetan
+ */
+@XmlRootElement
+public class DoorStatusResponse extends AbstractResponse implements Serializable{
+
+	private boolean doorOpen;
+	private boolean alarmOn;
+
+	public DoorStatusResponse(boolean doorOpen, boolean alarmOn) {
+		this.doorOpen = doorOpen;
+		this.alarmOn = alarmOn;
+	}
+
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
+
+	public void setDoorOpen(boolean doorOpen) {
+		this.doorOpen = doorOpen;
+	}
+
+	public boolean isAlarmOn() {
+		return alarmOn;
+	}
+
+	public void setAlarmOn(boolean alarmOn) {
+		this.alarmOn = alarmOn;
+	}
+}
