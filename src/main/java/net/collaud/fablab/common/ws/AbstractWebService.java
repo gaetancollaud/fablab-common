df0fe8f3f73447baf7af478ecdcce94836f61eb9
Gaétan Collaud <gaetancollaud@gmail.com>
2014-06-27 22:57:32 +0200

    Add token for webservice

--- src/main/java/net/collaud/fablab/common/ws/AbstractWebService.java
+++ src/main/java/net/collaud/fablab/common/ws/AbstractWebService.java
@@ -6,11 +6,15 @@ package net.collaud.fablab.common.ws;
  */
 abstract public class AbstractWebService {
 	
+	public static final String DEFAULT_TOKEN = "higuys!";
+	
 	protected void checkToken(String token){
 		//FIXME Get "token" from config file
-		if(token==null || !token.equals("salut")){
+		if(token==null || !token.equals(getToken())){
 			throw new RuntimeException("Wrong token : "+token);
 		}
 	}
+	
+	abstract protected String getToken();
 	
 }
